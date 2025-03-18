d2ba942dd7601b56d4fb59f19b0296a43fa46cc9
Meron T. Gebru <meronteklehaymanotgebru@gmail.com>
2025-03-18 06:26:59 +0300

    Add changes oon variable names based on assignment instruction

--- strings.js
+++ strings.js
@@ -1,28 +1,32 @@
-let extravaganzaStr = "extravaganza";
-let lastFourChars = extravaganzaStr.slice(-4);
+const extravaganza = "extravaganza";
+let lastFourChars = extravaganza.slice(-4);
 console.log("Last four characters:", lastFourChars);
-let sentenceStr = "The quick fox jumped over the lazy dog";
-let insertedStr = sentenceStr.slice(0, 4) + "eat" + sentenceStr.slice(4);
+
+const food = "The quick fox jumped over the lazy dog";
+let insertedStr = food.slice(0, 4) + "eat" + food.slice(4);
 console.log("Inserted string:", insertedStr);
-let storyStr = "The quick brown fox jumps over the lazy dog";
-let theOccurrences = (storyStr.match(/\bthe\b/gi) || []).length;
-let brownOccurrences = (storyStr.match(/\bbrown\b/g) || []).length;
+
+const story = "The quick brown fox jumps over the lazy dog";
+let theOccurrences = (story.match(/\bthe\b/gi) || []).length;
+let brownOccurrences = (story.match(/\bbrown\b/g) || []).length;
 console.log("Occurrences of 'the':", theOccurrences);
 console.log("Occurrences of 'brown':", brownOccurrences);
-let libraryStr = "The pupils are reading in the library";
-let tableStr = "The child was sitting on the table before it fell";
-let foundAre = libraryStr.match(/\bare\b/)[0];
-let foundSitting = tableStr.match(/\bsitting\b/)[0];
-console.log("Found word in libraryStr:", foundAre);
-console.log("Found word in tableStr:", foundSitting);
-let wonderfulStr = "wonderful";
-console.log("UpperCase:", wonderfulStr.toUpperCase());
-let amazingStr = "amazing";
-let underneathStr = "UndERneath";
-console.log("LowerCase (amazing):", amazingStr.toLowerCase());
-console.log("LowerCase (UndERneath):", underneathStr.toLowerCase());
-let titleStr = "A wonderful world";
-let titleCaseStr = titleStr
+
+const string1 = "The pupils are reading in the library";
+const string2 = "The child was sitting on the table before it fell";
+let foundAre = string1.match(/\bare\b/)[0];
+let foundSitting = string2.match(/\bsitting\b/)[0];
+console.log("Found word in string1:", foundAre);
+console.log("Found word in string2:", foundSitting);
+
+const wonderful = "wonderful";
+console.log("UpperCase:", wonderful.toUpperCase());
+const amazing = "amazing";
+const underneath = "UndERneath";
+console.log("LowerCase (amazing):", amazing.toLowerCase());
+console.log("LowerCase (UndERneath):", underneath.toLowerCase());
+const title = "A wonderful world";
+let titleCaseStr = title
   .toLowerCase()
   .split(" ")
   .map(word => word.charAt(0).toUpperCase() + word.slice(1))
